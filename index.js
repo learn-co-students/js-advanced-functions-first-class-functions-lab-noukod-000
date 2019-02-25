b32fc5aa0cba4347f9d2af85398b868435307e1e
Merline Belizaire <merline@noukod.com>
2019-02-25 17:41:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (drivers) {
+  return drivers.slice(0, 2);
+};
+
+returnFirstTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])
+
+const returnLastTwoDrivers = function (drivers) {
+  return drivers.slice(-2);
+};
+
+returnLastTwoDrivers(['Sally', 'Bob', 'Freddy', 'Claudia'])
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+const createFareMultiplier = function (multiplyValue) {
+  return function(value) {
+    return multiplyValue * value
+  }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, arg) {
+  return arg(drivers);
+}
